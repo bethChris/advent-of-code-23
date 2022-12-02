342161ce2692c3c96b90dca0f80a769a0c50e6e6
bethChris <bethiehc@gmail.com>
2022-12-01 23:01:08 -0700

    finished day2 pt 2

--- days/day2/tournyScore.py
+++ days/day2/tournyScore.py
@@ -10,25 +10,42 @@
     # add to total the points you playedd
 
 
+# if theres a tie, play score is same as opponent play score
+# if you need to beat them, 
+    # score is 2 if opponent was A, 3 if opponent was B, and 1 if opponent was C
+#if you need to lose,
+    # score is 3 if opponent was A, 1 if opponent was B and 2 if opponent was C
+
+
 
 import sys
 
 
-points = {
-    'X' : 1,
-    'Y' : 2,
-    'Z' : 3 
+play = {
+    'X' : 0,
+    'Y' : 3,
+    'Z' : 6 
 }
 
-beats = {
-    'X' : 'C',
-    'Y' : 'A',
-    'Z' : 'B',
-
-    'A' : 'Z',
-    'B' : 'X',
-    'C' : 'Y'
+win = {
+    'A': 2,
+    'B': 3,
+    'C': 1
 }
+
+lose = {
+    'A' : 3,
+    'B' : 1,
+    'C' : 2
+}
+
+tie = {
+    'A' : 1,
+    'B' : 2,
+    'C' : 3
+}
+
+
 
 
 data = sys.argv[1]
@@ -39,16 +56,16 @@ score = 0
 for line in file:
     if line.strip(): 
         opponent = line.split()[0]
-        me = line.split()[1]
+        outcome = line.split()[1]
 
-        if beats[me] is opponent:
-            score += 6
-        elif (beats[opponent] is me):
-            score += 0
+        if outcome == 'X':
+            score += lose[opponent]
+        elif outcome == 'Z':
+            score += win[opponent]
         else:
-            score += 3
-
-        score += points[me]
+            score += tie[opponent]
+    
+        score += play[outcome]
 
 
 print(score)
