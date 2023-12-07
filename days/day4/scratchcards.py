5c2c5a6c27946fb9965af6bd9f1703aba802acd2
Bethany Christensen <a02299515@aggies.usu.edu>
2023-12-06 17:20:48 -0700

    day 4 part 2 complete

--- days/day4/scratchcards.py
+++ days/day4/scratchcards.py
@@ -2,7 +2,7 @@ def parseInput(file):
     scratchcards_data = {}
     for line in file:
         line = line.strip()
-        card_id = line.replace("Card", "").split(":")[0]
+        card_id = line.replace("Card", "").split(":")[0].strip()
 
         number_stats = line.replace("Card", "").split(":")[1].split("|")
         
@@ -25,6 +25,32 @@ def calculate_points(card):
     
     return points
 
+
+def calculate_matches(card):
+    match_count = 0
+
+    for num in card["my_nums"]:
+        if num in card["winning_nums"]:
+            match_count += 1
+    
+    return match_count
+
+
+def sum_total_cards(scratchcards_data):
+    total_cards = 0
+    multipliers = {k: 1 for k in scratchcards_data.keys()}
+    for idx, card in scratchcards_data.items():
+        current_card_id = int(idx)
+        matches = calculate_matches(card)
+
+        for i in range(matches):
+            card_to_update = str(i+1+current_card_id)
+            multipliers[card_to_update] += multipliers[idx] #add 1 for every copy of this card that is produced
+
+
+    return sum(multipliers.values())
+
+
 def main():
     f = open("puzzle_input.txt")
     scratchcards_data = parseInput(f)
@@ -33,8 +59,10 @@ def main():
     for card in scratchcards_data.values():
         sum_of_points += calculate_points(card)
 
+    total_cards = sum_total_cards(scratchcards_data)
 
     print("Sum of all Card Points: ", sum_of_points)
+    print("Sum of total Cards Won: ", total_cards)
 
 
 main()
